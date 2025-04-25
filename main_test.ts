79a80721da86573eb0b64c52f80e604f04d48c62
GaryDev <112358055+garydev10@users.noreply.github.com>
2025-04-25 23:52:48 +0100

    add edit task test cases

--- main_test.ts
+++ main_test.ts
@@ -9,10 +9,29 @@ Deno.test("Homepage renders successfully", async () => {
   assertStringIncludes(html, "Flownami");
 });
 
-Deno.test("Board shows three columns", async () => {
+Deno.test("Board shows three columns and edit", async () => {
   const response = await fetch("http://localhost:8081/board");
   const html = await response.text();
   assertStringIncludes(html, "To Do");
   assertStringIncludes(html, "Doing");
   assertStringIncludes(html, "Done");
+  assertStringIncludes(html, "Edit");
+});
+
+Deno.test("Header includes script.js", async () => {
+  const response = await fetch("http://localhost:8081/board");
+  const html = await response.text();
+  assertStringIncludes(html, '<script src="/script.js" defer></script>');
+});
+
+Deno.test("Edit page includes task id and name", async () => {
+  const data = await Deno.readTextFile("./data.json");
+  const columns = JSON.parse(data);
+  const testTask = columns[0].tasks[0];
+  const response = await fetch(
+    `http://localhost:8081/tasks/edit/${testTask.id}`,
+  );
+  const html = await response.text();
+  assertStringIncludes(html, testTask.id);
+  assertStringIncludes(html, testTask.name);
 });
