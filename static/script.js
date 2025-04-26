1869bbc31e9d592a16bb9ca6a6eb6c5775fae747
GaryDev <112358055+garydev10@users.noreply.github.com>
2025-04-26 22:54:58 +0100

    rev to edit in place to align with PR #25

--- static/script.js
+++ static/script.js
@@ -1,10 +1,56 @@
 function addEditTaskEvent() {
-  const editTaskForms = document.querySelectorAll(".edit-task-form");
-  editTaskForms.forEach((form) => {
-    form.addEventListener("submit", async (event) => {
+
+  const enableTaskEdit = (itemId) => {
+    const editButton = document.querySelector(`.edit-button[data-id="${itemId}"]`);
+    const nameDisplay = document.querySelector(`.name-display[data-id="${itemId}"]`);
+    const nameEdit = document.querySelector(`.name-edit[data-id="${itemId}"]`);
+    const okButton = document.querySelector(`.ok-button[data-id="${itemId}"]`);
+    const cancelButton = document.querySelector(`.cancel-button[data-id="${itemId}"]`);
+    editButton.style.display = "none";
+    nameDisplay.style.display = "none";
+    nameEdit.style.display = "block";
+    okButton.style.display = "inline-block";
+    cancelButton.style.display = "inline-block";
+  }
+
+  const disableTaskEdit = (itemId) => {
+    const editButton = document.querySelector(`.edit-button[data-id="${itemId}"]`);
+    const nameDisplay = document.querySelector(`.name-display[data-id="${itemId}"]`);
+    const nameEdit = document.querySelector(`.name-edit[data-id="${itemId}"]`);
+    const okButton = document.querySelector(`.ok-button[data-id="${itemId}"]`);
+    const cancelButton = document.querySelector(`.cancel-button[data-id="${itemId}"]`);
+    editButton.style.display = "block";
+    nameDisplay.style.display = "block";
+    nameEdit.style.display = "none";
+    nameEdit.value = nameDisplay.innerText;
+    okButton.style.display = "none";
+    cancelButton.style.display = "none";
+  }
+
+  const editTaskButtons = document.querySelectorAll(".edit-button");
+  editTaskButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
       event.preventDefault();
       const itemId = event.target.getAttribute("data-id");
-      const inputTaskName = event.target.querySelectorAll(".edit-task-name")[0];
+      enableTaskEdit(itemId);
+    });
+  });
+
+  const cancelTaskButtons = document.querySelectorAll(".cancel-button");
+  cancelTaskButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+      const itemId = event.target.getAttribute("data-id");
+      disableTaskEdit(itemId);
+    });
+  });
+
+  const okTaskButtons = document.querySelectorAll(".ok-button");
+  okTaskButtons.forEach((button) => {
+    button.addEventListener("click", async (event) => {
+      event.preventDefault();
+      const itemId = event.target.getAttribute("data-id");
+      const inputTaskName = document.querySelector(`.name-edit[data-id="${itemId}"]`);
       const taskName = decodeURIComponent(inputTaskName.value);
       const url = "/tasks";
       try {
@@ -24,6 +70,7 @@ function addEditTaskEvent() {
       globalThis.location = "/board";
     });
   });
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
